9772cfb567b6bfbed80ac06d609c56c6c60ec0df
lambeboluwatife <boluwatifelambe@gmail.com>
2024-01-22 22:55:02 +0100

    hero page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,25 @@ const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <h1>Hello, I'm Sobodu Samuel Segun</h1>
+        <div className="grid">
+          <div className="bio">
+            <div className="container">
+              <h1>
+                <span>Hello,</span> I'm Sobodu Samuel Segun
+              </h1>
+              <h2>
+                I am a product manager with a result-oriented mindset with over
+                4+ years of experience in product marketing. Expertise in
+                driving successful product launches, creating compelling
+                messaging, and collaborating cross-functionally in Fintech and
+                Web3 organizations
+              </h2>
+            </div>
+          </div>
+          <div className="bio-image">
+            <img src="Segun1.jpg" alt="Segun's Picture" />
+          </div>
+        </div>
       </div>
     </div>
   );
